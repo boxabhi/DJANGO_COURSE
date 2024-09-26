c8ce017ed4607d1ab469cc2ddc1f7d310f57d8c5
Abhijeet Gupta <abhijeetg40@gmail.com>
2024-09-26 19:49:26 +0530

    pizza

--- PROJECTS/Pizza/home/consumer.py
+++ PROJECTS/Pizza/home/consumer.py
@@ -4,6 +4,41 @@ from .models import Pizza, Order
 from asgiref.sync import async_to_sync, sync_to_async
 
 
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = "hhhjuj"
+        self.room_group_name = "vbgbh"
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
+        self.accept()
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "chat.message", "message": message}
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({"message": message}))
+
 class OrderProgress(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['order_id']
--- PROJECTS/Pizza/pizza/asgi.py
+++ PROJECTS/Pizza/pizza/asgi.py
@@ -12,18 +12,26 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizza.settings')
 from django.urls import path
-from home.consumer import OrderProgress
+from home.consumer import OrderProgress,ChatConsumer
 
 application = get_asgi_application()
 
-ws_pattern = [
-        path("ws/pizza/<order_id>/", OrderProgress),
-]
+# ws_pattern = [
+#         path("ws/pizza/<order_id>/", OrderProgress),
+# ]
 
+# application = ProtocolTypeRouter({
+#     "websocket": (
+#         (
+#             URLRouter(ws_pattern)
+#         )
+#     ),
+# })
+
+django_asgi_app = get_asgi_application()
+ws_pattern=[path("ws/main/", ChatConsumer),]
 application = ProtocolTypeRouter({
-    "websocket": (
-        (
-            URLRouter(ws_pattern)
-        )
-    ),
+    "http": django_asgi_app,
+    'websocket': URLRouter(ws_pattern)
+    # Just HTTP for now. (We can add other protocols later.)
 })
